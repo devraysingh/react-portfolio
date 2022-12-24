5f8bdec8c3ea1180d4ff9139705a7ec704c36708
devraysingh <raysahni98@gmail.com>
2022-12-23 21:36:12 -0500

    Update index.js
    e

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
               <AiFillYoutube />
             </div>
             <div className='mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden  mt-20 md:h-96 md:w-96'>
-              <Image src={deved} fill objectFit="cover" alt=""/>
+              <Image src={deved} fill objectFit="cover" alt={""}/>
             </div>
           </div>
         </section>
